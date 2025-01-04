87826bcdef8eed00a4aff47b1cb411a222fd337f
adi-1205 <adityavyas70435@gmail.com>
2025-01-05 04:37:23 +0530

    Tailwind project setup and homepage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        f0f0f0: '#f0f0f0', // Custom color added
+        f2f0f1: '#f2f0f1', // Custom color added
+        f0eeed: '#f0eeed', // Custom color added
+      },
+    },
+  },
+  plugins: [],
+}
+
